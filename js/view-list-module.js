ccc01b67bf772bca43918b1d385c5831653d41d4
likedemian <likedemian@me.com>
2017-10-25 20:56:17 +0900

    [Add] List Module 클릭 시 목록 스타일 변화 기능 추가

---
+++ js/view-list-module.js
@@ -0,0 +1,7 @@
+$('.list-icon').on('click', function(){
+  $('.body__cont').addClass('list__form');
+})
+
+$('.module-icon').on('click', function(){
+  $('.body__cont').removeClass('list__form');
+})
